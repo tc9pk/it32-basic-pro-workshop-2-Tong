26393c9affb92b04e6cc31bef33dbc651ca11627
tanpak <thanyaphak2551@gmail.com>
2026-07-14 16:16:59 +0700

    workshop1

---
+++ workshop1.py
@@ -0,0 +1,6 @@
+quantity = input("Enter the quantity: ")
+cost_price = input("Enter the cost price: ")
+sell_price = input("Enter the selling price: ")
+team_member = input("Enter the team member's name: ")
+
+print(int(quantity) * float(sell_price))
